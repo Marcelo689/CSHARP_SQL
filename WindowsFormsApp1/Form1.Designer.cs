3d65ef583d0e9ad67c31b4e507d064e1dda63227
Marcelo <marcelosj7159@gmail.com>
2021-11-26 05:11:57 -0300

    Adicionando comando sql

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ButtonConsultar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonConsultar
+            // 
+            this.ButtonConsultar.Location = new System.Drawing.Point(337, 255);
+            this.ButtonConsultar.Name = "ButtonConsultar";
+            this.ButtonConsultar.Size = new System.Drawing.Size(75, 23);
+            this.ButtonConsultar.TabIndex = 0;
+            this.ButtonConsultar.Text = "Consultar";
+            this.ButtonConsultar.UseVisualStyleBackColor = true;
+            this.ButtonConsultar.Click += new System.EventHandler(this.Consultar_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.ButtonConsultar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form";
+            this.Text = "Aplicativo";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonConsultar;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -10,11 +12,40 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void Consultar_Click(object sender, EventArgs e)
+        {
+            string con = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+
+            MySqlConnection conexao = new MySqlConnection(con);
+            
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("Se conectoou com sucesso ao banco de dados", "Alerta");
+                MySqlCommand comando = conexao.CreateCommand();
+                comando.CommandText = "Select * from usuario where id = 1";
+                var reader = comando.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    MessageBox.Show(reader["nome"].ToString());
+                }
+            }
+            catch (MySqlException erro)
+            {
+                MessageBox.Show("Erro ao conectar " + erro.Message,"Alerta");
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
     }
 }
